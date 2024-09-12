f3145c91ab961806acdb5c8fc3be2c8d6d20f515
Ricardo Garza <rgarza0416@yahoo.com>
2024-09-12 17:44:24 -0500

    created log in functions

--- ToDoListApp/ViewModels/LoginViewViewModel.swift
+++ ToDoListApp/ViewModels/LoginViewViewModel.swift
@@ -5,19 +5,35 @@
 //  Created by Ricardo Garza on 9/11/24.
 //
 
+import FirebaseAuth
 import Foundation
 
 class LoginViewViewModel: ObservableObject {
     @Published var email = ""
     @Published var password = ""
+    @Published var errorMessage = ""
     
     init () {}
     
     func login() {
-        
+        guard validate() else {
+            return
+        }
+        Auth.auth().signIn(withEmail: email, password: password)
     }
     
-    func validate() {
-        
+    private func validate() -> Bool {
+        errorMessage = ""
+        guard !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+            
+            errorMessage = "Please fill in all fields."
+            return false
+        }
+        guard email.contains("@") && email.contains(".") else {
+            errorMessage = "Please enter valid email."
+            return false
+        }
+        return true
     }
 }
