1bca02dd6b8d5e8a110a67d04f84b16cea70521d
Ricardo Garza <rgarza0416@yahoo.com>
2024-09-13 15:52:16 -0500

    account UI setup

--- ToDoListApp/Views/MainView.swift
+++ ToDoListApp/Views/MainView.swift
@@ -13,11 +13,26 @@ struct MainView: View {
     
     var body: some View {
         if viewModel.isSignedIn, !viewModel.currentUserId.isEmpty {
-            //signed in
-            ToDoListView()
+            accountView
         } else {
             LoginView()
         }
+    }
+    
+    @ViewBuilder
+    var accountView: some View {
+        TabView {
+            ToDoListView(userId: viewModel.currentUserId)
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            
+            ProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: "person.circle")
+                }
+        }
+
     }
 }
 
