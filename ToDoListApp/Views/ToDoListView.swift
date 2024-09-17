8390286c69625d1d3eee61364e34b51221a07c08
Ricardo Garza <rgarza0416@yahoo.com>
2024-09-17 16:53:47 -0500

    Item list view updated

--- ToDoListApp/Views/ToDoListView.swift
+++ ToDoListApp/Views/ToDoListView.swift
@@ -5,22 +5,30 @@
 //  Created by Ricardo Garza on 9/11/24.
 //
 
+import FirebaseFirestoreSwift
 import SwiftUI
 
 struct ToDoListView: View {
     @StateObject var viewModel = ToDoListViewViewModel()
-    
-    private let userId: String
+    @FirestoreQuery var items: [ToDoListItem]
     
     init(userId: String) {
-        self.userId = userId
-        
+        self._items = FirestoreQuery(collectionPath: "users/\(userId)/todos")
     }
     
     var body: some View {
         NavigationView {
             VStack {
-                
+                List(items) { item in
+                    ToDoListItemView(item: item)
+                        .swipeActions {
+                            Button("Delete") {
+                                viewModel.delete(id: item.id)
+                            }
+                            .background(Color.red)
+                        }
+                }
+                .listStyle(PlainListStyle())
             }
             .navigationTitle("To Do List")
             .toolbar {
@@ -38,5 +46,5 @@ struct ToDoListView: View {
 }
 
 #Preview {
-    ToDoListView(userId: "")
+    ToDoListView(userId: "gegixLRRx4crZx3UGKV8A6dnb9M2")
 }
